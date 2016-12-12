2948c704b4431003ae63c955ff9c1de499e48571
apprenti <nans.mourgues@hotmail.fr>
2016-12-12 11:50:26 +0100

    Server setting

--- src/database.js
+++ src/database.js
@@ -7,7 +7,7 @@ let mongoose = require('mongoose');
 
 
 //connect method of mongoose connect to mongodb: with an if/else function for console messages in nodemon
-mongoose.connect('mongodb://localhost/bdx_entrepreneur', function(err) {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bdx_entrepreneur', function(err) {
     if (err) {
         console.log('Failed when trying to connect to Mongodb!');
     } else {
