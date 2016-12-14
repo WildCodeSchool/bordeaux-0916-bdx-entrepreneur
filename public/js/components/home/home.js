336fd1793938dfa8a69b9e7370dc3c401cb6159e
alexis.bordais@gmail.com <alexis.bordais@gmail.com>
2016-12-14 17:26:33 +0100

    middleware for authentification created

--- public/js/components/home/home.js
+++ public/js/components/home/home.js
@@ -1,13 +1,20 @@
 ((app) => {
+  'use strict'
     app.component('home', {
         templateUrl: 'js/components/home/home.html',
-        controller: function(contactsService) {
+        controller: ['contactsService', '$state', function(contactsService, $state) {
+//fonction connect for authentification for ng-click
+                // this.connect() {
+                //   contactsService.connect(this.user).then((user) => {
+                //     $state.go('app.company')
+                //   })
+                // }
 
                 this.carouselstate = 3
                 this.loadMore = () => {
                     this.carouselstate += 3
 
                 };
-          }
+          }]
     }); //dont delete
 })(require('angular').module('app.home'))
