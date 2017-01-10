aef0d86cd1c025000daa69e2d7396729d530d2df
krm22 <krmouat22@gmail.com>
2017-01-10 12:38:46 +0100

    added chips

--- public/js/components/admin/adminAddItem.js
+++ public/js/components/admin/adminAddItem.js
@@ -1,37 +1,40 @@
 ((app) => {
-    app.component('adminAddItem', {
-        templateUrl: 'js/components/admin/adminAddItem.html',
-        controller: function($stateParams, companiesService, $state, $mdToast) {
+        app.component('adminAddItem', {
+                templateUrl: 'js/components/admin/adminAddItem.html',
+                controller: function($stateParams, companiesService, $state, $mdToast) {
 
-            this.saveCompanies = (company) => {
-                companiesService.save(company).then((res) => {
-                    //Si c'est new company alors $state.go item + id du nouveau
-                    $mdToast.show(
-                        $mdToast.simple()
-                        .textContent('Simple Toast!')
-                        .position("bottom right")
-                        .hideDelay(3000)
-                    );
-                    if ($stateParams.id === '_new') {
-                        $state.go('company.item', {
-                            id: res.data._id
+                    this.saveCompanies = (company) => {
+                        companiesService.save(company).then((res) => {
+                            //Si c'est new company alors $state.go item + id du nouveau
+                            $mdToast.show(
+                                $mdToast.simple()
+                                .textContent('Simple Toast!')
+                                .position("bottom right")
+                                .hideDelay(3000)
+                            );
+                            if ($stateParams.id === '_new') {
+                                $state.go('company.item', {
+                                    id: res.data._id
+                                });
+                            }
+                        }).catch(function() {
+
                         });
-                    }
-                }).catch(function() {
-                  
-                });
-            };
-            this.createCompanies = (company) => {
-                companiesService.save(company).then((res) => {
-                    console.log("Your Contact Has Been Saved and Updated");
-                });
-            };
+                    };
+                    this.createCompanies = (company) => {
+                        companiesService.save(company).then((res) => {
+                            console.log("Your Contact Has Been Saved and Updated");
+                        });
+                    };
 
-            this.editMode = (company, index) => {
-              this.company.editMode = true;
-              this.company.editMode.onblur = this.saveCompanies;
-            };
+                    this.editMode = (company, index) => {
+                        this.company.editMode = true;
+                        this.company.editMode.onblur = this.saveCompanies;
+                    };
 
-          }
-        }); //dont delete
-})(require('angular').module('app.admin'))
+                    this.tags = [];
+
+
+                        } //dont delete
+                }); //dont delete
+        })(require('angular').module('app.admin'))
