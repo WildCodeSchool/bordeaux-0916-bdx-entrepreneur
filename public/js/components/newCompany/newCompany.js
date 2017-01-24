24ba08bc1f6876f2f7286780575010887deb2d73
sbstnplcn <sebastien.palacin@gmail.com>
2017-01-24 22:34:13 +0100

    new company

--- public/js/components/newCompany/newCompany.js
+++ public/js/components/newCompany/newCompany.js
@@ -4,20 +4,22 @@
         controller: ['$stateParams', 'companiesService', '$state', function($stateParams, companiesService, $state) {
             angular.extend(this, {
                 $onInit() {
+                    this.infos = {}
                     this.contacts = []
                 },
-                saveCompanies(company, tags, image) {
+                saveCompanies(company, tags, image, contacts) {
                     companiesService.upload(this.image)
                     this.infos.company = company
-                    this.infos.company.image = `img/${this.image.name}`
+                    this.infos.company.image = `img/${this.image.name}` || ""
                     this.infos.company.tags = [tags]
+                    this.infos.company.contacts = [contacts]
                     companiesService.add(this.infos).then((res) => {
                         this.newCompany = res.data
-                        console.log(res.data);
-                        $state.go("app.company({id: this.newCompany._id})")
-                    }).catch(() => {
-
-                    });
+                        console.log(res.data)
+                        // $state.go("app.company({id: this.newCompany._id})")
+                    }).catch((err) => {
+                        console.log(err)
+                    })
                 },
                 addUser(){
                     this.contacts.push({})
