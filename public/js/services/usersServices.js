c1f550bdd04e1cd001a46cde0661027f1ae780d5
jbehuet <jbehuet@gmail.com>
2017-01-31 09:31:21 +0100

    Rename file directive

--- public/js/services/usersServices.js
+++ public/js/services/usersServices.js
@@ -50,7 +50,6 @@
                             return this.disconnect()
                         this.getPopulate(payload._doc._id).then((res) => {
                             this.currentUser = res.data
-                            console.log(this.currentUser)
                             deferred.resolve(this.currentUser)
                         })
                     } else {
