2ab7bf7f318e1250ccc4ef13d7fcf9f4e20b2a7c
sbstnplcn <sebastien.palacin@gmail.com>
2017-01-31 09:54:30 +0100

    fusion page adherent & dashboard

--- public/js/components/adherents/adherents.md.js
+++
@@ -1,9 +0,0 @@
-  ((app) => {
-      'use strict'
-      app.config(['$stateProvider', ($stateProvider) => {
-          $stateProvider.state('app.adherents', {
-              url: '/adherents',
-              template: '<adherents></adherents>'
-          })
-      }])
-  })(angular.module('app.adherents', ['ui.router']));
